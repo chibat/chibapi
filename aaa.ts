c7cad33b6a6c72efefe883a69f2a3369a924a8b0
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2022-02-20 17:55:46 +0900

    dns

---
+++ aaa.ts
@@ -0,0 +1,4 @@
+
+const a = await Deno.resolveDns("golang.org", "A");
+console.log(a);
+
--- main.ts
+++ main.ts
@@ -1,21 +1,53 @@
-#!/usr/bin/env -S deno run --allow-net=:8000
+#!/usr/bin/env -S deno run --allow-net
 // http://localhost:8000/
 
 import { ConnInfo, serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-function handler(req: Request, connInfo: ConnInfo) {
+async function resolveDns(
+  query: string,
+  recordType: "A" | "AAAA" | "ANAME" | "CNAME" | "PTR",
+) {
+  try {
+    return await Deno.resolveDns(query, recordType);
+  } catch (e) {
+    console.log(e);
+  }
+  return [];
+}
+
+async function handler(req: Request, connInfo: ConnInfo) {
   const url = new URL(req.url);
-  if (url.pathname == "/ip") {
+  if (url.pathname === "/ip") {
     const addr = connInfo.remoteAddr as Deno.NetAddr;
     return new Response(JSON.stringify({ ip: addr.hostname }), {
       headers: { "content-type": "application/json; charset=utf-8" },
     });
-  } else {
-    return new Response("{}", {
-      status: 400,
-      headers: { "content-type": "application/json; charset=utf-8" },
-    });
+  } else if (url.pathname === "/dns") {
+    const query = url.searchParams.get("query");
+    if (query) {
+      const a = await resolveDns(query, "A");
+      const aaaa = await resolveDns(query, "AAAA");
+      const aname = await resolveDns(query, "ANAME");
+      const cname = await resolveDns(query, "CNAME");
+      const ptr = await resolveDns(query, "PTR");
+      return new Response(
+        JSON.stringify({
+          A: a,
+          AAAA: aaaa,
+          ANAME: aname,
+          CNAME: cname,
+          PTR: ptr,
+        }),
+        {
+          headers: { "content-type": "application/json; charset=utf-8" },
+        },
+      );
+    }
   }
+  return new Response("{}", {
+    status: 400,
+    headers: { "content-type": "application/json; charset=utf-8" },
+  });
 }
 
 serve(handler);
